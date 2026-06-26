326089fbabc4ea04ec4a5439664a675c8a2b60b3
massemiso <massemiso@proton.me>
2026-06-26 14:11:27 -0300

    [PERSONAL-BLOG] frontend: change date presentation & add link to home in article.html

--- personal-blog/backend/src/main/java/org/duckdns/massemiso/personal_blog/article/ArticleMapper.java
+++ personal-blog/backend/src/main/java/org/duckdns/massemiso/personal_blog/article/ArticleMapper.java
@@ -1,15 +1,17 @@
 package org.duckdns.massemiso.personal_blog.article;
 
+import java.time.format.DateTimeFormatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArticleMapper {
   public ArticleResponseDto toDto(Article entity) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, YYYY");
     return new ArticleResponseDto(
         entity.getId(),
         entity.getTitle(),
         entity.getContent(),
-        entity.getDateOfPublication().toLocalDate());
+        entity.getDateOfPublication().toLocalDate().format(formatter));
   }
 
   public Article toEntity(ArticleRequestDto dto) {
--- personal-blog/backend/src/main/java/org/duckdns/massemiso/personal_blog/article/ArticleResponseDto.java
+++ personal-blog/backend/src/main/java/org/duckdns/massemiso/personal_blog/article/ArticleResponseDto.java
@@ -1,5 +1,3 @@
 package org.duckdns.massemiso.personal_blog.article;
 
-import java.time.LocalDate;
-
-public record ArticleResponseDto(Long id, String title, String content, LocalDate dop) {}
+public record ArticleResponseDto(Long id, String title, String content, String dop) {}
