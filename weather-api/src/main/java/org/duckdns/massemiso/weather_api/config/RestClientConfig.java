0c297035b59b041fb1df2c6ea581aebf0029e6c6
massemiso <massemiso@proton.me>
2026-07-06 16:03:12 -0300

    [WEATHER-API] config: add timeout for read & connect to external weather API

--- weather-api/src/main/java/org/duckdns/massemiso/weather_api/config/RestClientConfig.java
+++ weather-api/src/main/java/org/duckdns/massemiso/weather_api/config/RestClientConfig.java
@@ -1,15 +1,26 @@
 package org.duckdns.massemiso.weather_api.config;
 
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
+  @Value("${restclient.connect.timeout}")
+  private Integer connectTimeout;
+  @Value("${restclient.read.timeout}")
+  private Integer readTimeout;
+
   @Bean
   public RestClient.Builder restClientBuilder() {
-    return RestClient.builder();
+    SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    factory.setConnectTimeout(Duration.ofSeconds(connectTimeout * 1000L));
+    factory.setReadTimeout(Duration.ofSeconds(readTimeout * 1000L));
+    return RestClient.builder().requestFactory(factory);
   }
 }
 
